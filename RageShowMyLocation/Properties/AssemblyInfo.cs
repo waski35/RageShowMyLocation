ec1b5759b7fb61ddb5c378b8f118f78599c63081
MarcinKalat <mkalat@mkalat.pl>
2016-01-11 21:29:58 +0100

    added displaying current peed and direction

--- RageShowMyLocation/Properties/AssemblyInfo.cs
+++ RageShowMyLocation/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("waski35")]
 [assembly: AssemblyProduct("RageShowMyLocation")]
-[assembly: AssemblyCopyright("Copyright © waski35 2015")]
+[assembly: AssemblyCopyright("Copyright © waski35 2015 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.31.0458")]
-[assembly: AssemblyFileVersion("0.2.31.0458")]
+[assembly: AssemblyVersion("0.3.11.2029")]
+[assembly: AssemblyFileVersion("0.3.11.2029")]
--- RageShowMyLocation/main_class.cs
+++ RageShowMyLocation/main_class.cs
@@ -187,6 +187,8 @@ namespace RageShowMyLocation
             street = street + ", " + GetCounty(street);
             street = street + ", " + GetSpeedLimit(street);
             street = street + " - Time - " + GetCurTime();
+            street = street + "| " + GetDirection() + " |";
+            street = street + "Speed : " + GetPlayerSpeed();
             if (option_developer > 0)
             {
                 street = street + ", POS - X : " + Convert.ToString(pl_pos.X) + ", Y : " + Convert.ToString(pl_pos.Y) + ", Z : " + Convert.ToString(pl_pos.Z);
@@ -1001,7 +1003,64 @@ namespace RageShowMyLocation
 
             return county;
         }
-       
+       private static String GetDirection ()
+        {
+            String direction = "";
+            Ped ped = Game.LocalPlayer.Character;
+            float heading_degrees = ped.Heading;
+            if ((heading_degrees > 350.0  && heading_degrees <= 0.0) || (heading_degrees >= 0.0 && heading_degrees < 15.0))
+            {
+                direction = "N";
+            }
+            else if (heading_degrees > 15.0 && heading_degrees < 80.0)
+            {
+                direction = "NE";
+            }
+            else if (heading_degrees > 80.0 && heading_degrees < 105.0)
+            {
+                direction = "E";
+            }
+            else if (heading_degrees > 105.00 && heading_degrees < 165.0)
+            {
+                direction = "SE";
+            }
+            else if (heading_degrees > 165.0 && heading_degrees < 195.0)
+            {
+                direction = "S";
+            }
+            else if (heading_degrees > 195.0 && heading_degrees < 255.0)
+            {
+                direction = "SW";
+            }
+            else if (heading_degrees > 255.0 && heading_degrees < 285.0)
+            {
+                direction = "W";
+            }
+            else if (heading_degrees > 285.0 && heading_degrees < 350.0)
+            {
+                direction = "NW";
+            }
+
+            return direction;
+        }
+        private static String GetPlayerSpeed()
+       {
+           String speed = "";
+           Ped ped = Game.LocalPlayer.Character;
+           float speed_meters = ped.Speed;
+           float speed_kmh = (speed_meters * Convert.ToSingle(3.6));
+            if (option_metric == 1)
+            {
+                speed = Convert.ToString(speed_kmh);
+            }
+            else
+            {
+                double speed_mph = (speed_kmh * Convert.ToSingle(0.621371192));
+                speed_mph = Math.Round(speed_mph,1);
+                speed = Convert.ToString(speed_mph);
+            }
+           return speed;
+       }
 
     }
 }
