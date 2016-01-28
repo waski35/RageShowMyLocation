46aa2a23d44a26ca7b92431a41ac6dfa7410410d
MarcinKałat <marcin.kalat@hotmail.com>
2016-01-28 14:23:18 +0100

    added bare class for ui

---
+++ RageShowMyLocation/PositionControl.cs
@@ -0,0 +1,17 @@
+﻿using Rage;
+using System;
+using System.IO;
+using System.Drawing;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RageShowMyLocation
+{
+    class PositionControl
+    {
+
+    }
+}
