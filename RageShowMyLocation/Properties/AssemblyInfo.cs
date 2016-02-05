1b108f943c6b9e0bf1dc199f92316bdffffde9bf
MarcinKałat <marcin.kalat@hotmail.com>
2016-02-05 19:25:38 +0100

    zmiana detekcji kalwiszy

--- RageShowMyLocation/Properties/AssemblyInfo.cs
+++ RageShowMyLocation/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.5.1756")]
-[assembly: AssemblyFileVersion("0.4.5.1756")]
+[assembly: AssemblyVersion("0.4.5.1825")]
+[assembly: AssemblyFileVersion("0.4.5.1825")]
