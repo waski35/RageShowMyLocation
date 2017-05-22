ffe2dbff2defc034d1b988b23faa6b2de4ce2bc0
MarcinKałat <mkalat@mkalat.pl>
2017-05-22 20:49:03 +0200

    changes in version applied

--- RageShowMyLocation/Properties/AssemblyInfo.cs
+++ RageShowMyLocation/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.171.19.1955")]
-[assembly: AssemblyFileVersion("0.171.19.1955")]
+[assembly: AssemblyVersion("0.172.20.2048")]
+[assembly: AssemblyFileVersion("0.172.20.2048")]
